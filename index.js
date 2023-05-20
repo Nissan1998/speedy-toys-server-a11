c70cafb2ff358334414cabfbe351ea965404ccdb
Nissan1998 <jnnissanm@gmail.com>
2023-05-21 04:24:06 +0600

    limitedDataFor20

--- index.js
+++ index.js
@@ -42,7 +42,7 @@ async function run() {
     });
 
     app.get("/alltoys", async (req, res) => {
-      const result = await toysCollection.find({}).toArray();
+      const result = await toysCollection.find({}).limit(20).toArray();
       res.send(result);
     });
   } finally {
