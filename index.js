c208897175f60300e913e3dbe0c9a376dbca1847
Nissan1998 <jnnissanm@gmail.com>
2023-05-21 20:50:59 +0600

    deleteAPICreated

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 5000;
@@ -56,6 +56,15 @@ async function run() {
       const result = await toysCollection.insertOne(body);
       res.send(result);
       console.log(result);
+    });
+
+    // Delete Toy
+
+    app.delete("/myToys/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await toysCollection.deleteOne(query);
+      res.send(result);
     });
   } finally {
     // Ensures that the client will close when you finish/error
